cc4d8f68e48897ce10099c26dbc5b196da0af961
mrgamer <mister.gamer@gmail.com>
2015-09-04 02:44:24 +0200

    delegated express structure to its own directory

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-'use strict';
-
-let chalk = require('chalk');
-
-let // local
-  ioc = require('./ioc'),
-  app = ioc.create('app');
-
-app.set('port', process.env.PORT || 8080);
-app.set('hostname', process.env.HOSTNAME || '127.0.0.1');
-
-let server = app.listen(app.get('port'), app.get('hostname'), function () {
-  console.log(chalk.green('Express server listening on:'), `http://${server.address().address}:${server.address().port}/`);
-});
+// index should return files inside components dir
---
+++ structure/README.md
@@ -0,0 +1,11 @@
+# Structure
+
+This directory illustrates a plausible express.js application structure: 
+
+  * handling multiple configuration
+  * using different environment variables to customize behaviour
+  * having both API and server-side rendered pages
+  * serving documentation for the APIs
+
+Begin your read from [`index.js`](structure/index.js) and go backwards for the full ride.
+
