c7f7416abb8759ecf9bd0f738fd38f24bbc11045
mrgamer <mister.gamer@gmail.com>
2015-09-24 12:27:35 +0200

    crypto now produces shorter strings

--- components/crypto.js
+++ components/crypto.js
@@ -11,14 +11,14 @@ let crypto = require('crypto');
 exports = module.exports = function (config) {
   function encrypt (text) {
     let cipher = crypto.createCipher(config.crypto.algorithm, config.crypto.secret);
-    let crypted = cipher.update(text, 'utf8', 'hex');
-    crypted += cipher.final('hex');
+    let crypted = cipher.update(text, 'utf8', 'base64');
+    crypted += cipher.final('base64');
     return crypted;
   }
 
   function decrypt (text) {
     let decipher = crypto.createDecipher(config.crypto.algorithm, config.crypto.secret);
-    let dec = decipher.update(text, 'hex', 'utf8');
+    let dec = decipher.update(text, 'base64', 'utf8');
     dec += decipher.final('utf8');
     return dec;
   }
